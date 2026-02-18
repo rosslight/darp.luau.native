13c3f8002c2281ef7ab54b235449c5b8fe3a652a
OleRoss <ole.rosskamp@rosslight.de>
2026-02-18 21:17:15 +0100

    fix: Mark lua_error as obsolete to avoid misuse

--- src/Darp.Luau.Native/LuauNativeMethods.cs
+++ src/Darp.Luau.Native/LuauNativeMethods.cs
@@ -4,6 +4,8 @@ namespace Darp.Luau.Native;
 
 static unsafe partial class LuauNative
 {
+    /// <seealso href="https://learn.microsoft.com/en-us/dotnet/standard/native-interop/exceptions-interoperability"/>
+    [Obsolete("Do not use lua_error from managed code. The dotnet clr does not support longjmp!")]
     [DllImport(__DllName, EntryPoint = "lua_error", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
     public static extern void lua_error(lua_State* L);
 }
